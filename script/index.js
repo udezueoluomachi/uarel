81d1d554801b9018ec22ea4ee2c8c900bf89a66c
udezueoluomachi <74429592+udezueoluomachi@users.noreply.github.com>
2023-01-15 15:14:47 +0100

    added noscript text

---
+++ script/index.js
@@ -0,0 +1,67 @@
+
+        const $ = e => document.querySelector(e);
+        const urlInput = $("#url-input");
+        const genBtn = $("button.input");
+
+        genBtn.addEventListener("click", () => {
+            genBtn.innerText = "...";
+            genBtn.disabled = true;
+            let dataToServer = urlInput.value.trim();
+
+            function reset() {
+                urlInput.value = "";
+                genBtn.innerText = "Generate";
+                genBtn.disabled = false;
+            }
+
+            function request() {
+                fetch("http://localhost:4000/", {
+                    method : "POST",
+                    body : dataToServer
+                })
+                .then(data => data.json())
+                .then(res => {
+                    let url = "https://github.com/udezueoluomachi/uarel";//change this to the response url
+                    swal("Generated", url, {
+                        icon : "success",
+                        buttons: {
+                            cancel : "close",
+                            confirm : {
+                                text : "copy",
+                                value : true,
+                                closeModal : false
+                            }
+                        }
+                    })
+                    .then(val => {
+                        reset();
+                        navigator.clipboard.writeText(url)
+                        swal({
+                            title : "Copied!"
+                        })
+                    })
+                })
+                .catch(err => {
+                    swal({
+                        title : "An error occured",
+                        icon : "error",
+                        buttons : {
+                            cancel : "close",
+                            retry : {
+                                text : "retry",
+                                closeModal : false
+                            }
+                        }
+                    })
+                    .then(val => {
+                        if(val) {
+                            request();
+                        }
+                        else {
+                            reset();
+                        }
+                    })
+                })
+            }
+            request();
+        })
